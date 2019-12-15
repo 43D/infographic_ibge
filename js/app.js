2df168a07394e3bcbd4dddc68a854218a9cb1606
Allan Fel <allan.goioere@hotmail.com>
2019-12-15 18:32:15 -0300

    teste

--- js/app.js
+++ js/app.js
@@ -8,6 +8,27 @@ var json = $.getJSON("https://raw.githubusercontent.com/43D/infographic_ibge/mas
     main(json)
 });
 
+function loadJSON(callback) {
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'js/ibge.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+function init() {
+    loadJSON(function(response) {
+
+        var actual_JSON = JSON.parse(response);
+        console.log(actual_JSON);
+    });
+}
+console.log(json);
 
 function loadMap() {
     $('#brazil-map').JSMaps({
